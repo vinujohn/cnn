29d77f4310e815520679d3a285dc380a38932b83
Vinu John <vinu_john@hotmail.com>
2023-12-11 19:46:45 -0500

    fix download script

--- fc/train/download.sh
+++ fc/train/download.sh
@@ -5,17 +5,17 @@ trainingLabels="train-labels-idx1-ubyte"
 testImages="t10k-images-idx3-ubyte"
 testLabels="t10k-labels-idx1-ubyte"
 
-baseURL1="https://web.archive.org/web/20220331225332/http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
-baseURL2="https://web.archive.org/web/20220331225243/http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
-baseURL3="https://web.archive.org/web/20220331225223if_/http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
-baseURL4="https://web.archive.org/web/20220331225222if_/http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
+trainImagesURL="https://web.archive.org/web/20220331225332/http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
+trainLabelsURL="https://web.archive.org/web/20220331225243/http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
+testImagesURL="https://web.archive.org/web/20220331225223if_/http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
+testLabelsURL="https://web.archive.org/web/20220331225222if_/http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
 
 outputDir="./fc/train/data" 
 
-wget $baseURL -O ${outputDir}/${trainingImages}.gz ${baseURL1}
-wget $baseURL -O ${outputDir}/${trainingLabels}.gz ${baseURL2}
-wget $baseURL -O ${outputDir}/${testImages}.gz ${baseURL3}
-wget $baseURL -O ${outputDir}/${testLabels}.gz ${baseURL4}
+wget $trainImagesURL -O ${outputDir}/${trainingImages}.gz
+wget $trainLabelsURL -O ${outputDir}/${trainingLabels}.gz
+wget $testImagesURL -O ${outputDir}/${testImages}.gz
+wget $testLabelsURL -O ${outputDir}/${testLabels}.gz
 
 gzip -d ${outputDir}/${trainingImages}.gz
 gzip -d ${outputDir}/${trainingLabels}.gz
